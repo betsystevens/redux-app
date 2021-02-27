7f8b8637a740c91b921d5d14069ee47d98aeda95
betsystevens <betsystevens5@gmail.com>
2021-02-26 21:57:33 -0600

    add state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 import HelloWorld from "./components/HelloWorld";
 
 function App() {
+  const [tech] = useState("Redux");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HelloWorld tech="Redux" />
+        <HelloWorld tech={tech} />
       </header>
     </div>
   );
